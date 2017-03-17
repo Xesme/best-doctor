98742ec5fe96c704cc30ce53e279b97929f1a4bb
xa <xiaesmeamen@me.com>
2017-03-17 12:15:39 -0700

    set up API $.get in doctor.js and set up jquery in doctor-interface.js

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,16 @@
-var apiKey = require('./.env').apiKey;
+// var apiKey = require('./.env').apiKey;
+var Doctor = require('./../js/doctor.js').Doctor;
+
+$(document).ready(function(){
+  var availableDoctor = new Doctor();
+  $('#search-from').submit(function(event) {
+    event.preventDefault();
+    var ailment = $('#ailment').val();
+    var specialty = $('#specialty').val();
+    var location = $('#location').val();
+    var gender = $('#gender').val();
+    console.log(ailment);
+    var results = availableDoctor.searchDoctor(ailment, specialty, location, gender);
+    // $('#results').append("<li>" + results + "</li>");
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,14 @@
 var apiKey = require('./.env').apiKey;
+
+Doctor = function(){
+}
+
+Doctor.prototype.searchDoctor(ailment, specialty, location, gender) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&location=' + location + '&gender=' + gender + '&limit=10&user_key=' + apiKey).then(function(response){
+    // console.log(response);
+  }).fail(function(error){
+    console.log('fail');
+  });
+};
+
+exports.doctorModule = Doctor;
