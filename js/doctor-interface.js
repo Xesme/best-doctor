03fb22d21df596b5d6456618b5d28c2e883d0a1c
xa <xiaesmeamen@me.com>
2017-03-17 16:54:14 -0700

    uncomment out code used for build debugging

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,14 @@
-// var Doctor = require('./../js/doctor.js').doctorModule;
-//
-// $(document).ready(function(){
-//   $('#search-form').submit(function(event){
-//     event.preventDefault();
-//     var ailment = $('#ailment').val();
-//     var specialty = $('#specialty').val();
-//     var location = $('#location').val();
-//     var gender = $('#gender').val();
-//     var newDoctor = new Doctor();
-//     newDoctor.searchDoctor(ailment);
-//     $('#results').text('submit');
-//     });
-// });
+var Doctor = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function(){
+  $('#search-form').submit(function(event){
+    event.preventDefault();
+    var ailment = $('#ailment').val();
+    var specialty = $('#specialty').val();
+    var location = $('#location').val();
+    var gender = $('#gender').val();
+    var newDoctor = new Doctor();
+    newDoctor.searchDoctor(ailment);
+    $('#results').text('submit');
+    });
+});
